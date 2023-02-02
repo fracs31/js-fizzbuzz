574e9de9eb7889d332f3f8fda1909a58c62f2d39
fracs31 <pist.fracs95@gmail.com>
2023-02-02 15:02:10 +0100

    ADD print of the messages with the numbers

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
 //Ciclo di stampa
-for (let i = 0; i < 30; i++) {
-
-    console.log((i + 1), ((i + 1) % 3));
-    console.log((i + 1), ((i + 1) % 5));
-    console.log((i + 1), ((i + 1) % 15));
-
-    if (((i + 1) % 3) == 0) {
-        console.log("Divisibile per 3: " + (i + 1));
-    }
-    if (((i + 1) % 5) == 0) {
-        console.log("Divisibile per 5: " + (i + 1));
-    }
-    if (((i + 1) % 15) == 0) {
-        console.log("Divisibile per 15: " + (i + 1));
+for (let i = 0; i < 100; i++) {
+    //Se il numero è divisibile solo per 3, ma non per 5
+    if (((i + 1) % 3) == 0 && ((i + 1) % 5) != 0) {
+        console.log("Fizz: " + (i + 1)); //stampa
+    } else if (((i + 1) % 3) != 0 && ((i + 1) % 5) == 0) { //Se il numero è divisibile solo per 5, ma non per 3
+        console.log("Buzz: " + (i + 1)); //stampa
+    } else if (((i + 1) % 3) == 0 && ((i + 1) % 5) == 0) { //Se il numero è divisibile sia per 3 sia per 5
+        console.log("FizzBuzz: " + (i + 1)); //stampa
     }
 }
